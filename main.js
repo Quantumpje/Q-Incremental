4ce71459cba0fd38ad0b091594ac1d778fca1c30
Quantumpje <121110274+Quantumpje@users.noreply.github.com>
2024-10-21 11:59:54 +0200

    v0.01

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+const unsupportedVersions = [0]
+
 var saveData = {
     points: 0,
     upgrades: 0,
-    update: 0,
+    autoclickers: 0,
+    update: 0.01,
 }
 var defSave = saveData
 
@@ -17,6 +20,10 @@ if (savegame !== null && savegame.update == saveData.update) {
             }
         }
     }
+}
+
+if (unsupportedVersions.includes(saveData.update)) {
+    DelSave()
 }
 
 var saveGameLoop = window.setInterval(function () {
@@ -40,12 +47,20 @@ function display_Upgrades() {
     document.getElementById("Upgrades").innerHTML = `${formatNum(saveData.upgrades)} Upgrades`
 }
 
+function display_AutoClickers() {
+    document.getElementById("AutoClickers").innerHTML = `${formatNum(saveData.autoclickers)} Auto Clickers`
+}
+
 function display_PointsButton() {
     document.getElementById("PointsButton").innerHTML = `${formatNum(formula_points())} Points Button`
 }
 
 function display_UpgradesButton() {
     document.getElementById("UpgradesButton").innerHTML = `${formatNum(formula_upgrade_P())} Points >>> ${formatNum(formula_upgrade_U())} Upgrades Button`
+}
+
+function display_BuyAutoClickerButton() {
+    document.getElementById("BuyAutoClickerButton").innerHTML = `${formatNum(formula_buyautoclicker_U())} Upgrades >>> ${formatNum(formula_buyautoclicker_CL())} Auto Clickers Button`
 }
 
 
@@ -59,21 +74,37 @@ function cooldown_UpgradesButton() {
     setTimeout(function () { document.getElementById("UpgradesButton").disabled = false }, 100)
 }
 
+function cooldown_BuyAutoClickerButton() {
+    document.getElementById("BuyAutoClickerButton").disabled = true
+    setTimeout(function () { document.getElementById("BuyAutoClickerButton").disabled = false }, 100)
+}
+
 
 function formula_points() {
-    return 1 * 1.25 ** saveData.upgrades
+    return saveData.upgrades > 1 ? 1 * 1.25 ** saveData.upgrades : 1
 }
 
 function formula_upgrade_U() {
     return 1
 }
 function formula_upgrade_P() {
-    return 10 * 1.35 ** saveData.upgrades
+    return saveData.upgrades > 1 ? 10 * 1.35 ** saveData.upgrades : 10
+}
+
+function formula_buyautoclicker_CL() {
+    return 1
+}
+function formula_buyautoclicker_U() {
+    return saveData.autoclickers > 1 ? 8 * 1.25 ** (saveData.autoclickers) : 8
 }
 
 
 function check_upgrade() {
     return saveData.points >= formula_upgrade_P()
+}
+
+function check_autoclicker() {
+    return saveData.upgrades >= formula_buyautoclicker_U()
 }
 
 
@@ -99,6 +130,20 @@ function Upgrade() {
     }
 }
 
+function BuyAutoClicker() {
+    if (check_autoclicker()) {
+        saveData.upgrades -= formula_buyautoclicker_U()
+        saveData.autoclickers += formula_buyautoclicker_CL()
+
+        display_Upgrades()
+        display_AutoClickers()
+        display_UpgradesButton()
+        display_BuyAutoClickerButton()
+
+        cooldown_BuyAutoClickerButton()
+    }
+}
+
 
 function DelSave() {
     saveData = defSave
@@ -113,12 +158,25 @@ window.setInterval(function () {
     } else {
         document.getElementById("UpgradesButton").disabled = true
     }
+    if (check_autoclicker()) {
+        document.getElementById("BuyAutoClickerButton").disabled = false
+    } else {
+        document.getElementById("BuyAutoClickerButton").disabled = true
+    }
 }, 100)
 
 
-setTimeout(function () {
+window.setInterval(function () {
+    saveData.points += formula_points() * (saveData.autoclickers / 2)
+    display_Points()
+}, 1000)
+
+
+window.setTimeout(function () {
     display_Points()
     display_PointsButton()
     display_Upgrades()
     display_UpgradesButton()
+    display_AutoClickers()
+    display_BuyAutoClickerButton()
 }, 50)
